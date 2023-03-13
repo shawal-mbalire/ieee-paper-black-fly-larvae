2aafc1a0db053644312ba4dfe9b0e3de57bb622c
Shawal Mbalire <mbalireshawal@gmail.com>
2023-03-13 08:26:41 +0300

    Update espCode.ino

--- espCode/espCode.ino
+++ espCode/espCode.ino
@@ -5,39 +5,52 @@ IEEE Paper Black Soldier Fly BSF
 #include <DHT.h>
 #include <Firebase_ESP_Client.h>
 #include <WiFi.h>
+#include <BH1750.h>
+#include <Wire.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
 #define WIFI_SSID     "TP-Link_659A"
 #define WIFI_PASSWORD "98781997"
-#define DATABASE_URL "https://ieeepaper-default-rtdb.europe-west1.firebasedatabase.app");
-#define API_KEY "AIzaSyAHDUls-v3DKM7q9X70OkXaMzIbNgWqHR4"
+#define DATABASE_URL  "https://ieeepaper-default-rtdb.europe-west1.firebasedatabase.app"
+#define API_KEY       "AIzaSyAHDUls-v3DKM7q9X70OkXaMzIbNgWqHR4"
+#define USER_EMAIL    "esp32s@bsf.com"
+#define USER_PASSWORD "password"
 
 #define DHTTYPE   DHT22
-#define DHTPIN    2
-#define ldrPin    3
-#define heaterPin 4
-#define fogPin    5
+#define DHTPIN    5
+#define ldrPin    4
+#define heaterPin 3
+#define fogPin    2
 #define shadePin  6
+//#define bh1750_sda 21
+//#define bh1750_scl 22
 
-DHT dht(DHTPIN, DHTTYPE);
-FirebaseData fbdo;
-FirebaseAuth auth;
+DHT            dht(DHTPIN, DHTTYPE);
+BH1750         lightMeter(0x23);
+FirebaseData   fbdo;
+FirebaseAuth   auth;
 FirebaseConfig config;
+FirebaseJson   temjson;
+FirebaseJson   luxjson;
+FirebaseJson   humjson;
+WiFiUDP        ntpUDP;
+NTPClient      timeClient(ntpUDP);
 
 const float   GAMMA              = 0.7;
 const float   RL10               = 50;
 unsigned long sendDataPrevMillis = 0;
 bool          signupOK           = false;
-float         firetemperature    = "";
-float         firehumidity       = "";
-int           firelux            = "";
-String        fireFogPump        = "";
-String        fireShadeMotor     = "";
-String        fireHeater         = "";
-
-auth.user.email    = "esp32s@bsf.com"
-auth.user.password = "password"
+bool          setFog;
+bool          setShade;
+bool          setBulb;
+float         lux;
+int           splitT;
+String        formattedDate;
+String        dayStamp;
+String        timeStamp;
 
 void setup(){
     pinMode(ldrPin,     INPUT);
@@ -45,9 +58,20 @@ void setup(){
     pinMode(heaterPin, OUTPUT);
     pinMode(shadePin,  OUTPUT);
     pinMode(fogPin,    OUTPUT);
+
+    Wire.begin();
     dht.begin();
+    timeClient.begin();
     Serial.begin(115200);
+    
+    timeClient.setTimeOffset(3600*3);
     Serial.println("Hello, Node MCU ESP-32!");
+    //===========================BH1750====================
+    if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+      Serial.println(F("BH1750 Advanced begin"));
+    } else {
+      Serial.println(F("Error initialising BH1750"));
+    }
 
     /*==============================================================================
     ================================================================================
@@ -72,9 +96,12 @@ void setup(){
     ===========================FireBase=============================================
     ================================================================================
     ================================================================================*/
-    config.api_key = API_KEY;
-    config.database_url=DATABASE_URL;
-    if {Firebase.signUp(&config,&auth,"","")}{
+    config.api_key     = API_KEY;
+    config.database_url= DATABASE_URL;
+    auth.user.email    = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    if (Firebase.signUp(&config,&auth,"","")){
       Serial.println("Sign up ok");
       signupOK = true;
     }else{
@@ -90,7 +117,19 @@ void loop()
 {
     //
     delay(5000);
-
+    /*==============================================================================
+    ================================================================================
+    ===========================Time================================================
+    ================================================================================
+    ================================================================================*/
+   
+    while(!timeClient.update()) {
+        timeClient.forceUpdate();
+      }
+    formattedDate = timeClient.getFormattedDate();
+    splitT        = formattedDate.indexOf("T");
+    dayStamp      = formattedDate.substring(0, splitT);
+    timeStamp     = formattedDate.substring(splitT+1, formattedDate.length()-1);
     /*==============================================================================
     ================================================================================
     ===========================DHT22================================================
@@ -98,7 +137,7 @@ void loop()
     ================================================================================*/
     float humidity    = dht.readHumidity();
     float temperature = dht.readTemperature();
-    if (isnan(humidity) || isnan(temperature) || isnan(fahrenheit)){
+    if (isnan(humidity) || isnan(temperature)){
         Serial.println(F("Failed to read from DHT sensor!"));
         return;
     }
@@ -107,99 +146,117 @@ void loop()
     Serial.print(F("%  Temperature: "));
     Serial.print(temperature);
     Serial.print(F("°C "));
-    Serial.print(fahrenheit);
-    Serial.print(F("°F  Heat index: "));
-    Serial.print(hic);
-    Serial.print(F("°C "));
-    Serial.print(hif);
-    Serial.println(F("°F"));
+
+
+    temjson.add("day",dayStamp);
+    temjson.add("time",timeStamp);
+    temjson.add("tem",temperature);
+
+
+    humjson.add("day",dayStamp);
+    humjson.add("time",timeStamp);
+    humjson.add("hum",humidity);
 
     /*==============================================================================
     ================================================================================
     ===========================Light================================================
     ================================================================================
     ================================================================================*/
-    // Convert the analog value into lux value:
+
+    // Convert the ldr analog value into lux value:
     int   analogValue = analogRead(ldrPin);
     float voltage     = (analogValue / 1024.) * 5;
     float resistance  = 2000 * voltage / (1 - voltage / 5);
-    float lux         = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
+    float lux_ldr     = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
     Serial.println(F("Intensity: "));
-    Serial.print(lux);
+    Serial.print(lux_ldr);
     Serial.print(F("lux"));
 
+    // ==========BH1750=========================
+    
+    if (lightMeter.measurementReady()) {
+      lux = lightMeter.readLightLevel();
+      Serial.print("Light: ");
+      Serial.print(lux);
+      Serial.println(" lx");
+    }
+     
+    luxjson.add("day",dayStamp);
+    luxjson.add("time",timeStamp);
+    luxjson.add("lux",lux);
 
     /*==============================================================================
     ================================================================================
     ===========================Heater===============================================
     ================================================================================
     ================================================================================*/
-    fireHeater = Firebase.getString("Heater");
-    if (fireHeater = "ON"){
-        Serial.println("Heater turned on");
-        digitalWrite(heaterPin,HIGH);
-    }else if (fireHeater = "OFF"){
-        Serial.println("Heater turned off");
-        digitalWrite(heaterPin,LOW);
+    if (Firebase.RTDB.getBool(&fbdo, "heaterBulb/setOn")) {
+        if (fbdo.dataType() == "boolean") {
+            setBulb = fbdo.boolData();
+            Serial.println("Successful READ from " + fbdo.dataPath() + ":" + String(setBulb) + " (" + fbdo.dataType() + " ");
+            //ledcWrite (PWMChannel, pwmValue);
+            digitalWrite(heaterPin, setBulb);
+        }
     }else{
-        Serial.println("command Error, please send fog ON/OFF")
+        Serial.println("FAILED: " + fbdo.errorReason ());
     }
     /*==============================================================================
     ================================================================================
     ===========================FogPump===============================================
     ================================================================================
     ================================================================================*/
-    fireFogPump = Firebase.getString("FogPump",    "OFF");
-    if (fireFogPump = "ON"){
-        Serial.println("Heater turned on");
-        digitalWrite(fogPin,HIGH);
-    }else if (fireFogPump = "OFF"){
-        Serial.println("Heater turned off");
-        digitalWrite(fogPin,LOW);
+    if (Firebase.RTDB.getBool(&fbdo, "fogMachine/setOn")) {
+        if (fbdo.dataType() == "boolean") {
+            setFog = fbdo.boolData();
+            Serial.println("Successful READ from " + fbdo.dataPath() + ":" + String(setFog) + " (" + fbdo.dataType() + " ");
+            //ledcWrite (PWMChannel, pwmValue);
+            digitalWrite(fogPin, setFog);
+        }
     }else{
-        Serial.println("command Error, please send fog ON/OFF")
+        Serial.println("FAILED: " + fbdo.errorReason ());
+    }
     /*==============================================================================
     ================================================================================
     ===========================Shade================================================
     ================================================================================
     ================================================================================*/
-    if (Firebase.RTDB.getBool(&fbdo, "shadeMotor/setShadeOff")) {
-        if (fbdo.dataType ()"bool") {
-            shadeValue = fbdo.boolData();
-            Serial.println("Successful READ from " + fbdo.dataPath() + ":" + shadeValue + " (" + fbdo.dataType() + " ");
+    if (Firebase.RTDB.getBool(&fbdo, "shadeMotor/setOnn")) {
+        if (fbdo.dataType() == "boolean") {
+            setShade = fbdo.boolData();
+            Serial.println("Successful READ from " + fbdo.dataPath() + ":" + String(setShade) + " (" + fbdo.dataType() + " ");
             //ledcWrite (PWMChannel, pwmValue);
-            digitalWrite(shadePin, shadeValue);
+            digitalWrite(shadePin, setShade);
         }
     }else{
         Serial.println("FAILED: " + fbdo.errorReason ());
     }
 
-    if (fireShadeMotor = "ON"){
-        Serial.println("Heater turned on");
-        digitalWrite(shadePin,HIGH);
-    }else if (fireShadeMotor = "OFF"){
-        Serial.println("Heater turned off");
-        digitalWrite(shadePin,LOW);
-    }else{
-        Serial.println("command Error, please send shade ON/OFF")
-
 
 
 
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
-    sendDataPrevMillis =` millis();
+    sendDataPrevMillis = millis();
     // -----------------STORE sensor data to a RTDB----------------------
-    //-------------pushing lux data to firebase----------------
-    if (Firebase.RTDB.setInt(&fbdo, "BH1750/lux", lux)){
-      Serial.println("LUX SENT");
+    //-------------pushing ldr lux data to firebase----------------
+    if (Firebase.RTDB.setInt(&fbdo, "LDR/lux", lux_ldr)){
+      Serial.println("LDR LUX SENT");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
     }else {
-      Serial.println("FAILED TO SEND LUX");
+      Serial.println("FAILED TO SEND LDR LUX");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    //-------------pushing lux data to firebase----------------
+    if (Firebase.RTDB.setInt(&fbdo, "BH1750/lux", lux)){
+      Serial.println("BH1750 LUX SENT");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }else {
+      Serial.println("FAILED TO SEND BH1750 LUX");
       Serial.println("REASON: " + fbdo.errorReason());
     }
     //-------------pushing humidity data to firebase----------------
-    if (Firebase.RTDB.setFloat(&fbdo, "DHT22/humidity", humidity)){
+    if (Firebase.RTDB.setFloat(&fbdo, "DHT/humidity", humidity)){
       Serial.println("HUMIDITY SENT");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
@@ -208,7 +265,7 @@ void loop()
       Serial.println("REASON: " + fbdo.errorReason());
     }
     //-------------pushing temperature data to firebase----------------
-    if (Firebase.RTDB.setFloat(&fbdo, "DHT22/temperature(celcius)", temperature)){
+    if (Firebase.RTDB.setFloat(&fbdo, "DHT/temperature(celcius)", temperature)){
       Serial.println("TEMPERATURE SENT");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
@@ -216,5 +273,57 @@ void loop()
       Serial.println("FAILED TO SEND TEMPERATURE");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-  }
+    //-------------pushing shade ison data to firebase----------------
+    if (Firebase.RTDB.setBool(&fbdo, "shadeMotor/isOn", digitalRead(shadePin))){
+      Serial.println("shadeMotor SENT");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }else {
+      Serial.println("FAILED TO SEND shadeMotor");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    //-------------pushing heater data to firebase----------------
+    if (Firebase.RTDB.setBool(&fbdo, "heaterBulb/isOn", digitalRead(heaterPin))){
+      Serial.println("heaterBulb SENT");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }else {
+      Serial.println("FAILED TO SEND heaterBulb");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    //-------------pushing fogMachine data to firebase----------------
+    if (Firebase.RTDB.setBool(&fbdo, "fogMachine/isOn", digitalRead(fogPin))){
+      Serial.println("fogMachine SENT");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }else {
+      Serial.println("FAILED TO SEND fogMachine");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+
+    if (Firebase.RTDB.pushJSON(&fbdo, "graph/temp", &temjson)) {
+      Serial.println(fbdo.dataPath());
+      Serial.println(fbdo.pushName());
+      Serial.println(fbdo.dataPath() + "/"+ fbdo.pushName());
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
+    
+    if (Firebase.RTDB.pushJSON(&fbdo, "graph/hum", &humjson)) {
+      Serial.println(fbdo.dataPath());
+      Serial.println(fbdo.pushName());
+      Serial.println(fbdo.dataPath() + "/"+ fbdo.pushName());
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
+
+
+    if (Firebase.RTDB.pushJSON(&fbdo, "graph/lux", &luxjson)) {
+      Serial.println(fbdo.dataPath());
+      Serial.println(fbdo.pushName());
+      Serial.println(fbdo.dataPath() + "/"+ fbdo.pushName());
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
 }
