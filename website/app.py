7d442f3dcf1e2555405fda82b3a8de4a7f57847c
Bradley Alfred Mpeera Tumwine <111291321+bradleytumwine@users.noreply.github.com>
2023-05-19 16:37:05 +0300

    Update app.py

--- website/app.py
+++ website/app.py
@@ -64,30 +64,79 @@ def login():
 
 # Main Page
 def main():
-    st.title("BSF System Parameters")
-    st.subheader("Main")
+    st.title("BSF System")
+    st.subheader("Parameters")
 
     # Get data from Firebase Realtime Database
     lux          = db.child("BH1750"    ).child("lux"        ).get()
     temperature  = db.child("DHT"       ).child("temperature").get()
     humidity     = db.child("DHT"       ).child("humidity"   ).get()
-    onFog        = db.child("fogMachine").child("isOn"       ).get()
-    onShade      = db.child("shadeMotor").child("isOn"       ).get()
-    onBulb       = db.child("heaterBulb").child("isOn"       ).get()
-    setFog       = db.child("fogMachine").child("setOn"      ).get()
-    setShade     = db.child("shadeMotor").child("setOn"      ).get()
-    setBulb      = db.child("heaterBulb").child("setOn"      ).get()
-
+    
     # Display data
     st.write("Lux: ",             lux.val())
     st.write("Temperature: ",     temperature.val())
     st.write("Humidity: ",        humidity.val())
-    st.write("Fog Machine: ",     onFog.val())
-    st.write("Shade Motor: ",     onShade.val())
-    st.write("Heater Bulb: ",     onBulb.val())
-    st.write("Set Fog Machine: ", setFog.val())
-    st.write("Set Shade Motor: ", setShade.val())
-    st.write("Set Heater Bulb: ", setBulb.val())
+    
+    st.subheader("Heater bulb")
+    if st.button('Heater Bulb On'):
+      db.child('heaterBulb').update({"setOn":True})
+    if st.button('Undo Heater Bulb On'):
+      db.child('heaterBulb').update({"setOn":False})
+    if st.button('Heater Bulb Off'):
+      db.child('heaterBulb').update({"setOff":True})
+    if st.button('Undo Heater Bulb Off'):
+      db.child('heaterBulb').update({"setOff":False})
+    else:
+      pass
+    
+    st.subheader("Fog Machine")
+    if st.button('Fog Machine On'):
+      db.child('fogMachine').update({"setOn":True})
+    if st.button('Undo Fog Machine On'):
+      db.child('fogMachine').update({"setOn":False})
+    if st.button('Fog Machine Off'):
+      db.child('fogMachine').update({"setOff":True})
+    if st.button('Undo Fog Machine Off'):
+      db.child('fogMachine').update({"setOff":False})
+    else:
+      pass
+    
+    st.subheader("Shade Motor")
+    if st.button('Shade Motor On'):
+      db.child('shadeMotor').update({"setOn":True})
+    if st.button('Undo Shade Motor On'):
+      db.child('shadeMotor').update({"setOn":False})
+    if st.button('Shade Motor Off'):
+      db.child('shadeMotor').update({"setOff":True})
+    if st.button('Undo Shade Motor Off'):
+      db.child('shadeMotor').update({"setOff":False})
+    else:
+      pass
+    
+    
+#     onFog        = db.child("fogMachine").child("isOn"       ).get()
+#     offFog       = db.child("fogMachine").child("isOff"      ).get()
+#     onShade      = db.child("shadeMotor").child("isOn"       ).get()
+#     offShade     = db.child("shadeMotor").child("isOff"      ).get()
+#     onBulb       = db.child("heaterBulb").child("isOn"       ).get()
+#     offBulb      = db.child("heaterBulb").child("isOff"      ).get()
+#     setFogOn     = db.child("fogMachine").child("setOn"      ).get()
+#     setFogOff    = db.child("fogMachine").child("setOff"     ).get()
+#     setShadeOn   = db.child("shadeMotor").child("setOn"      ).get()
+#     setShadeOff  = db.child("shadeMotor").child("setOff"     ).get()
+#     setBulbOn    = db.child("heaterBulb").child("setOn"      ).get()
+#     setBulbOff   = db.child("heaterBulb").child("setOff"     ).get()
+
+#     st.write("Fog Machine manual on: ",     onFog.val())
+#     st.write("Fog Machine manual off: ",     offFog.val())
+#     st.write("Shade Motor: ",     onShade.val())
+#     st.write("Heater Bulb: ",     onBulb.val())
+#     st.write("Set Fog Machine Off: ", setFogOn.val())
+#     st.write("Set Fog Machine : ", setFogOff.val())
+#     st.write("Set Shade Motor: ", setShade.val())
+#     st.write("Set Shade Motor: ", setShade.val())
+#     st.write("Set Heater Bulb: ", setBulbOn.val())
+#     st.write("Set Heater Bulb: ", setBulbOn.val())
 
 
 if __name__ == "__main__":
