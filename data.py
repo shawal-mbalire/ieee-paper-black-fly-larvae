5ff8c277c3dec5424fff6d00f0a497903890bbc2
Shawal Mbalire <mbalireshawal@gmail.com>
2023-05-26 12:32:33 +0300

    added data from bradley json file

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 import json
 
 
-with open('ieeepaper-default-rtdb-export.json') as f:
+with open('bsf-bradley-default-rtdb-export.json') as f:
     data = json.load(f)
     # convvvvert to dictionary
     data = dict(data)
@@ -17,19 +17,19 @@ hum =  dict(graph['hum']) # day hum time
 # Access data in lux
 for i in lux:
     #add values to lux.csv
-    with open('lux.csv', 'a') as f:
+    with open('lux2.csv', 'a') as f:
         f.write(str(lux[i]['day']) + ',' + str(lux[i]['time']) + ',' + str(lux[i]['lux']) + '\n')
 
 # Access data in temp
 for i in temp:
     #add values to temp.csv
-    with open('temp.csv', 'a') as f:
+    with open('temp2.csv', 'a') as f:
         f.write(str(temp[i]['day']) + ',' + str(temp[i]['time']) + ',' + str(temp[i]['tem']) + '\n')
 
 # Access data in hum
 for i in hum:
     #add values to hum.csv
-    with open('hum.csv', 'a') as f:
+    with open('hum2.csv', 'a') as f:
         f.write(str(hum[i]['day']) + ',' + str(hum[i]['time']) + ',' + str(hum[i]['hum']) + '\n')
 
 
